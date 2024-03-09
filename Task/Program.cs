3af03bf9a126f874c7d7118a2c315ea557e7ba99
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-03-09 14:45:19 +0300

    added the solution to the task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+string[] FilterArrayByLength(string[] array, int maxLength)
+{
+    int count = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= maxLength)
+        {
+            count++;
+        }
+    }
+
+    string[] newArray = new string[count];
+    int index = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= maxLength)
+        {
+            newArray[index] = item;
+            index++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (string item in array)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Console.Write("Введите элементы массива через пробел: ");
+string input = Console.ReadLine();
+string[] originalArray = input.Split(' ');
+
+//string[] originalArray = { "123", "test", "a", "longword", "ok" };
+string[] newArray = FilterArrayByLength(originalArray, 3);
+
+//Console.WriteLine("Исходный массив:");
+//PrintArray(originalArray);
+
+Console.WriteLine($"\nНовый массив (элементы длиной не более 3 символов): ");
+PrintArray(newArray);
+
